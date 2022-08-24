b1ccbc497f9c29e172cbe7704bce10f3f65d43fd
bc8c <bc8c@naver.com>
2022-08-24 02:05:32 +0000

    add comment

--- network/docker/testcreate.sh
+++ network/docker/testcreate.sh
@@ -84,6 +84,7 @@ export CORE_PEER_LOCALMSPID="Org4MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org4.example.com/users/Admin@org4.example.com/msp
 export CORE_PEER_ADDRESS=localhost:5004
+# fdkjflkdajfls
 
 set -x
 peer channel join -b ./channel-artifacts/${CHANNEL_NAME}.block >&log.txt
